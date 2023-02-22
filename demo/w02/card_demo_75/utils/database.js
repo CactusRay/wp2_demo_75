e2f4af89ffb6b0c23726c9d960c882b6b55dd795
CactusRay <b19670630@gmail.com>
2023-02-22 21:29:21 +0800

    w02 所有畫面更新

---
+++ demo/w02/card_demo_75/utils/database.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'wp2_demo_75',
+    password: '0000'
+})
+console.log('connect database', pool.options.database);
+
+module.exports = pool; 
